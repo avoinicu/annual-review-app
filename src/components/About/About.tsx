c33cf45b52f901959dd957fd2dfa111063564787
Adrian Voinicu <adrian.voinicu@coolblue.nl>
2025-01-01 11:14:16 +0100

    fix about drawer

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,6 @@ import { BadgeInfo } from 'lucide-react';
 
 import { showAboutAtom } from '~/atoms/showAbout.atom';
 import { Button } from '~/components/ui/button';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '~/components/ui/dialog';
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
 
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from '../ui/drawer';
@@ -12,23 +11,25 @@ const About = () => {
   const [showAbout, setShowAbout] = useAtom(showAboutAtom);
 
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DialogTrigger asChild>
-              <Button
-                onClick={() => setShowAbout(true)}
-                variant="outline"
-                size="icon"
-              >
-                <BadgeInfo className="h-[1.2rem] w-[1.2rem]" />
-              </Button>
-            </DialogTrigger>
-          </TooltipTrigger>
-          <TooltipContent>About this app</TooltipContent>
-        </Tooltip>
-      </DrawerTrigger>
+    <Drawer
+      open={showAbout}
+      onClose={() => setShowAbout(false)}
+    >
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <DrawerTrigger asChild>
+            <Button
+              onClick={() => setShowAbout(true)}
+              variant="outline"
+              size="icon"
+            >
+              <BadgeInfo className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          </DrawerTrigger>
+        </TooltipTrigger>
+        <TooltipContent>About this app</TooltipContent>
+      </Tooltip>
+
       <DrawerContent>
         <div className="w-full max-w-md mx-auto">
           <DrawerHeader>
@@ -73,65 +74,6 @@ const About = () => {
         </div>
       </DrawerContent>
     </Drawer>
-  );
-
-  return (
-    <Dialog
-      open={showAbout}
-      onOpenChange={(open) => {
-        setShowAbout(open);
-      }}
-    >
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <DialogTrigger asChild>
-            <Button
-              onClick={() => setShowAbout(true)}
-              variant="outline"
-              size="icon"
-            >
-              <BadgeInfo className="h-[1.2rem] w-[1.2rem]" />
-            </Button>
-          </DialogTrigger>
-        </TooltipTrigger>
-        <TooltipContent>About this app</TooltipContent>
-      </Tooltip>
-      <DialogContent aria-describedby={undefined}>
-        <DialogHeader>
-          <DialogTitle>What's this about?</DialogTitle>
-          <div id="about-content">
-            <p className="mb-2 dark:text-slate-400 text-slate-600">
-              I created this app out of boredom and existential dread during the 2024 holiday season. While listening to
-              one of Chris Williamson's podcasts, I heard him mention his{' '}
-              <a
-                className="underline dark:text-slate-200 text-slate-800"
-                href="https://chriswillx.com/review/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                annual review template
-              </a>
-              .
-            </p>
-            <p className="mb-2 dark:text-slate-400 text-slate-600">
-              Curious, I downloaded the template and thought it would be fun to turn it into a web app.
-            </p>
-          </div>
-          <p className="mt-4 mb-2 dark:text-slate-400 text-slate-600">
-            Rest assured,{' '}
-            <strong className="dark:text-slate-200 text-slate-800">I’m not storing any of your data</strong> beyond your
-            browser. It’s nothing personal—I’m just not that curious about your life. Unless, of course, we become
-            friends; then I’ll be all ears!
-          </p>
-          <p className="mt-4 mb-2 dark:text-slate-400 text-slate-600">
-            Once you finish your review, you can{' '}
-            <strong className="dark:text-slate-200 text-slate-800">download it as a PDF</strong> or{' '}
-            <strong className="dark:text-slate-200 text-slate-800">create a calendar event</strong> for next year with a
-            link to this year's review.
-          </p>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
   );
 };
 
